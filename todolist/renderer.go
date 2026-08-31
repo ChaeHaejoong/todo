b7037d59106956c393d3813623e03cebf2e0e7d3
ChaeHaejoong <svneh0329@naver.com>
2026-08-31 12:10:00 +0900

    feat: change render massage

--- todolist/renderer.go
+++ todolist/renderer.go
@@ -9,18 +9,17 @@ import (
 )
 
 func renderTodoList(content string, focused bool) string {
-	return ui.TitledBorder(100, 10, "Todo", content, focused)
+	return ui.TitledBorder(100, 10, "[1] Todo", content, focused)
 }
 
 func renderTodos(todos []store.Todo) string {
 	if len(todos) == 0 {
-		return "할 일이 없어요"
+		return "Nothing to do"
 	}
 
 	lines := make([]string, 0, len(todos))
-	for i, todo := range todos {
-
-		lines = append(lines, fmt.Sprintf("%d. %s", i+1, todo.Content))
+	for _, todo := range todos {
+		lines = append(lines, fmt.Sprintf("- %s", todo.Content))
 	}
 
 	return strings.Join(lines, "\n")
