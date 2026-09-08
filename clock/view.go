8b06ebcc7f27843fa10aafd5c4d61a8c5daac71d
ChaeHaejoong <svneh0329@naver.com>
2026-09-08 20:00:07 +0900

    feat: place clock center

--- clock/view.go
+++ clock/view.go
@@ -1,6 +1,9 @@
 package clock
 
-import "github.com/chaehaejoong/todo/internal/ui"
+import (
+	"charm.land/lipgloss/v2"
+	"github.com/chaehaejoong/todo/internal/ui"
+)
 
 func (m Model) View(width, height int, focused bool) string {
 	content := renderClock(
@@ -9,5 +12,14 @@ func (m Model) View(width, height int, focused bool) string {
 		m.cursor,
 		focused,
 	)
+	contentWidth, contentHeight := ui.TitledBorderContentSize(width, height)
+	content = lipgloss.Place(
+		contentWidth,
+		contentHeight,
+		lipgloss.Center,
+		lipgloss.Center,
+		content,
+	)
+
 	return ui.TitledBorder(width, height, "[3] Clock", content, focused)
 }
