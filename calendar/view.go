8277559f0935ec548329b22411fedcabee4a1b42
ChaeHaejoong <svneh0329@naver.com>
2026-09-08 19:59:48 +0900

    feat: place calendar center

--- calendar/view.go
+++ calendar/view.go
@@ -1,7 +1,19 @@
 package calendar
 
-import "github.com/chaehaejoong/todo/internal/ui"
+import (
+	"charm.land/lipgloss/v2"
+	"github.com/chaehaejoong/todo/internal/ui"
+)
 
 func (m Model) View(width, height int, focused bool) string {
-	return ui.TitledBorder(width, height, "[2] Calendar", m.renderCalendar(), focused)
+	contentWidth, contentHeight := ui.TitledBorderContentSize(width, height)
+	content := lipgloss.Place(
+		contentWidth,
+		contentHeight,
+		lipgloss.Center,
+		lipgloss.Center,
+		m.renderCalendar(),
+	)
+
+	return ui.TitledBorder(width, height, "[2] Calendar", content, focused)
 }
